b8973fa322be199e9a7a9b14f7e84e0cc00e4a26
Zack <zazi21@student.bth.se>
2026-01-06 15:48:39 +0100

    fix(login): Correct messages to match cipbia (#11)

--- data/scripts/creaturescripts/login.lua
+++ data/scripts/creaturescripts/login.lua
@@ -24,16 +24,20 @@ conditionLight:setParameter(CONDITION_PARAM_LIGHT_LEVEL, 9)
 local creatureevent = CreatureEvent("PlayerLogin")
 
 function creatureevent.onLogin(player)
-	local loginStr = "Welcome to " .. configManager.getString(configKeys.SERVER_NAME) .. "!"
+	local loginStr = ""
 	if player:getLastLoginSaved() <= 0 then
-		loginStr = loginStr .. " Please choose your outfit."
+		loginStr = string.format("Welcome to %s! Please choose your outfit.", configManager.getString(configKeys.SERVER_NAME))
 		player:sendOutfitWindow()
 	else
 		if loginStr ~= "" then
 			player:sendTextMessage(MESSAGE_STATUS_DEFAULT, loginStr)
 		end
 
-		loginStr = string.format("Your last visit was on %s.", os.date("%a %b %d %X %Y", player:getLastLoginSaved()))
+		loginStr = string.format(
+		"Your last visit in %s: %s.",
+		configManager.getString(configKeys.SERVER_NAME),
+		os.date("%d. %b %Y %X %Z", player:getLastLoginSaved())
+	)
 	end
 	player:sendTextMessage(MESSAGE_STATUS_DEFAULT, loginStr)
 
